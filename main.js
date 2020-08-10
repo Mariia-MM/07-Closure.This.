80ca7ccd04df7a04e655c06f65fc68e779bed85f
Mariia <mytsko.mariia@gmail.com>
2020-08-10 13:31:27 +0200

    functions#1-#3

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
 const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
 const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
+
+// // function#1
+
+// function getMyTaxes(salary) {
+//   myTaxes = this.tax * salary;
+//   return console.log(myTaxes);
+// }
+// getMyTaxes.call(latvia, 1000);
+
+// // function#2
+
+// function getMiddleTaxes(country) {
+//   middleTaxes = this.tax * this.middleSalary;
+//   return console.log(middleTaxes);
+// }
+// getMiddleTaxes.call(litva);
+
+// // function#3
+
+// function getTotalTaxes() {
+//   totalTaxes = this.tax * this.middleSalary * this.vacancies;
+//   return console.log(totalTaxes);
+// }
+// getTotalTaxes.call(ukraine);
+
+// function#4
+
+function getMySalary() {
+  salary=
+}
